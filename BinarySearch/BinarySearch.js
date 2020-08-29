5e3f7ead9d390af887f80a38c301bc80e9c4c248
Owen Jones <owenthefirst93@gmail.com>
2020-08-29 14:28:46 -0400

    binary search iterative

---
+++ BinarySearch/BinarySearch.js
@@ -0,0 +1,25 @@
+//Given a sorted (in ascending order) integer array nums of n elements and a target value, write a function to search target in nums. 
+//If target exists, then return its index, otherwise return -1.
+
+
+const binarySearch = (arr, target) => {
+    let low = 0
+    let high = arr.length - 1
+
+    while (low <= high){
+        let mid = Math.floor((low + high)/ 2)
+        let guess = arr[mid]
+
+        if (guess === target){
+            return mid
+        }else if(guess > target){
+            high = mid - 1
+        }else {
+            low = mid + 1 
+        }
+    }
+    return null
+}
+
+console.log(binarySearch([0,1,2,3,4,5,6,7,8], 0))
+console.log(binarySearch([0,1,2,3,4,5,6,7,8], 5))
