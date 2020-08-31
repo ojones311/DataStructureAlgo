5fb5338760f1d27533e0d6bcc210f402eba11c2d
Owen Jones <owenthefirst93@gmail.com>
2020-08-30 20:13:53 -0400

    pushed sorted and returned index

--- BinarySearch/SearchIndexPosition.js
+++ BinarySearch/SearchIndexPosition.js
@@ -4,26 +4,29 @@
 //Input: [1,3,5,6], 5
 //Output: 2
 
-const searchInsert = (arr, marker, target) => {
+const searchInsert = (arr, target) => {
     let low = 0
     let high = arr.length - 1
 
     while (low <= high){
-        let mid = Math.floor((low - high)/ 2)
+        let mid = Math.floor((low + high)/ 2)
         let guess = arr[mid]
-        let marker = null
 
-        if (guess === mid){
+        if (guess === target){
             return mid
         }else if(guess > target){
             high = mid - 1
-            
+
         }else if(guess < target){
             low = mid + 1 
         }
     }
-    return marker
+    arr.push(target)
+    arr.sort((a,b) => {return a - b})
+    console.log(arr)
+
+    return arr.indexOf(target)
 }
 
-searchInsert([0,1,3,5,6,8,12,21], 22)
-searchInsert([6,24,78,96,100,530], 40)
+console.log(searchInsert([0,1,3,5,6,8,12,21], 22))
+console.log(searchInsert([6,24,78,96,100,530], 40))
