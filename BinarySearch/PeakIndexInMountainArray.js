46f9240d7490e16229282113f7c726f73eb3d90c
Owen Jones <owenthefirst93@gmail.com>
2020-09-05 10:17:27 -0400

    peakindex of mountain array

---
+++ BinarySearch/PeakIndexInMountainArray.js
@@ -0,0 +1,6 @@
+// Given an integer array arr that is guaranteed to be a mountain, return any i such that 
+// arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
+
+var peakIndexInMountainArray = function(A) {
+    return A.indexOf(Math.max(...A))
+};
