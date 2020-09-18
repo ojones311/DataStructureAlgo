8fc74106bf0dc74f4edec8f2ce68199684ed6f14
Owen Jones <owenthefirst93@gmail.com>
2020-09-18 00:09:55 -0400

    started queue using ll

---
+++ Queues/QueueUsingLL.js
@@ -0,0 +1,42 @@
+class Node {
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+class LinkedList {
+    constructor(){
+        this.head = null
+    }
+    
+}
+
+class Queue {
+    constructor(){
+        this.first = null
+        this.last = null
+    }
+
+    enqueue(value){
+
+    }
+
+    dequeue(){
+
+    }
+
+    peek(){
+        return 
+    }
+
+    rear(){
+
+    }
+
+    size(){
+
+    }
+    isEmpty(){
+
+    }
+}
