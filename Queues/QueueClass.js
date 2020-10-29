754c582833386d4e70d937c290c2c3b95f10ae43
Owen Jones <owenthefirst93@gmail.com>
2020-10-28 20:36:01 -0400

    print queue

--- Queues/QueueClass.js
+++ Queues/QueueClass.js
@@ -10,7 +10,7 @@ class Queue {
     }
 
     dequeue(){
-        return this.arr.shift();
+        return this.elements.shift();
     }
 
     peek(){
@@ -24,7 +24,39 @@ class Queue {
     size(){
         return this.elements.length
     }
+
+    print(){
+        this.elements.map((elem) => console.log('elem:',elem))
+    }
 }
 
 
 let myQueue = new Queue()
+
+myQueue.enqueue('Owen')
+myQueue.enqueue('Alejo')
+myQueue.enqueue('Johanne')
+myQueue.enqueue('John')
+myQueue.enqueue('Nilber')
+
+console.log(myQueue)
+
+const printQueue = (q) => {
+    let temp = new Queue
+    let str = ''
+    while(q.size()){
+     let elem = q.dequeue()
+     str += '<-' + elem 
+     temp.enqueue(elem)
+   }
+   while(!temp.isEmpty()){
+       q.enqueue(temp.dequeue())
+   }
+   return str
+}
+
+console.log(printQueue(myQueue))
+
+myQueue.enqueue('Mom')
+
+console.log(printQueue(myQueue))
