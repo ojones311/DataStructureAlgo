1f0180bb90781467f9349139da610782867ae2ea
Owen Jones <owenthefirst93@gmail.com>
2021-02-24 16:06:12 -0500

    html entity parser

---
+++ HashTables/HTMLEntityParser.js
@@ -0,0 +1,20 @@
+//Return the text after replacing the entities by the special characters.
+
+const entityParser = function(text) {
+    let obj = {
+        '&quot;': '"',
+        '&apos;': "'",
+        '&amp;': '&',
+        '&gt;': '>',
+        '&lt;': '<',
+        '&frasl;': '/'
+    }
+    
+    let replacer = new RegExp(Object.keys(obj).join('|'),"gi")
+    text = text.replace(replacer,function(matched){
+        return obj[matched]
+    })
+    return text
+};
+
+consoel.log(entityParser('x &gt; y &amp;&amp; x &lt; y is always false'))
