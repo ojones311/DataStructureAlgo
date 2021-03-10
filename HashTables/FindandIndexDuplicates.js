d6714054f206fff9b8391618be088275eb02c0f8
Owen Jones <owenthefirst93@gmail.com>
2021-03-09 23:09:50 -0500

    findandindexdupes

--- HashTables/FindandIndexDuplicates.js
+++ HashTables/FindandIndexDuplicates.js
@@ -4,24 +4,25 @@
 //obj = {0:[0,1],1:[2],2:[3],3:[4],4:[5,6]}
 
 //gotta fix this !
-const findAndIndexDupes = (arr) => {
+
+const findDupes = (arr) => {
 	let obj = {}
-	for(let [i,elem] of arr.entries()){
-	    if(!obj[arr[elem]]){
-	        obj[arr[elem]] = []
-	        obj[arr[elem]].push(i)
-        } else {
-        obj[arr[elem]].push(i)
-    }
+	let newArr = []
+	for(let i = 0; i < arr.length; i++){
+		if(!obj[arr[i]]){
+			obj[arr[i]] = []
+			obj[arr[i]].push(i)
+		}else{
+			obj[arr[i]].push(i)
+		}
+	}
+	for(let elem in obj){
+		if(obj[elem].length > 1){
+			newArr.push({[elem]: obj[elem]})
+		}
+	}
+	return newArr
 }
-    for(let key in obj){
-	    if(obj[key].length >= 1){
-	        delete key
-        }
-    }
 
-return obj
-
-}
-
-console.log(findAndIndexDupes([1,1,2,2]))
+console.log(findDupes([0,1,1,1,1,2,33,3,3]))
+console.log(findDupes([1,1,2,2]))
--- HashTables/TwoSum.js
+++ HashTables/TwoSum.js
@@ -14,12 +14,11 @@ var twoSum = function(nums, target) {
     }
     return null
 };
+//arr = [0,1,2,3,4,5] target = 9
+//obj = {0:9, 1:8, 2:7, 3:6, 4:5, 5:4}
 
 
- 
-
-
-console.log(twoSum([2,7,11,15], 13))
+console.log(twoSum2([2,7,11,15], 13))
 console.log(twoSum([2,7,11,15], 12))
 console.log(twoSum([2,7,11,15], 22))
 console.log(twoSum([2,7,11,15], 30))
